ace3e61fe86b0e271d2dfa1be1233c2994410ebb
Tim Creasman <tjcreasm@ncsu.edu>
2020-06-29 14:55:56 -0400

    Added routes to three basic pages: landing, login, and list
    resolves #12

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
+});
+
+Route::get('/list', function () {
+    return view('list');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
