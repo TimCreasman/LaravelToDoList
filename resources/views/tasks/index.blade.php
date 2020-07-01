e791a622d55848670a2708dca0ced88d7a6d4cc0
Tim Creasman <tjcreasm@ncsu.edu>
2020-07-01 14:16:01 -0400

    Added sorting and filtering options to the index page
    A user can now view tasks based on their status or priority.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
     <h1>Tasks:</h1>
+    <div class="btn-group" role="group">
+        <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+            Sort options
+        </button>
+        <div class="dropdown-menu">
+            <a href="/tasks" class="dropdown-item"> View all tasks </a>
+            <a href="/tasks?completed=true" class="dropdown-item"> View complete tasks </a>
+            <a href="/tasks?completed=false" class="dropdown-item"> View incomplete tasks </a>
+        </div>
+    </div>
+    @if (request('priority'))
+        <h4 class="">
+            Filtering by: {{request('priority')}}
+        </h4>
+    @endif
     <div class="container-fluid d-flex flex-wrap" style="min-height: 10em">
         @foreach ($tasks as $task)
         <div class="card m-2" style="min-width: 20em; ">
@@ -9,19 +24,28 @@
                 <h3 class="card-text text-truncate">
                     {{$task->description}}
                 </h3>
-                <h6 class="card-subtitle {{$task->completed ? 'text-success' : 'text-muted'}}">
-                    {{$task->completed ? 'Task complete!' : ''}}
-                </h6>
+
+                {{-- If the task is not completed show the complete button --}}
                 @if (is_null($task->completed))
                     <form method="POST" action="{{$task->path()}}/complete">
                         @csrf
                         @method('PUT')
-                        <button type="submit" class="btn btn-success">Complete task</button>
+                        <button type="submit" class="btn btn-success my-2">Complete task</button>
                     </form>
+                @else
+                    <h6 class="card-subtitle text-success">
+                        Task complete!
+                    </h6>
                 @endif
+
                 <a class="btn btn-primary ml-auto" href="{{$task->path()}}">
                     View more...
                 </a>
+                <div>
+                    @foreach ($task->priorities as $priority)
+                    <a href="/tasks?priority={{$priority->type}}" class="btn btn-outline-secondary btn-sm">{{$priority->type}}</a>
+                    @endforeach
+                </div>
             </div>
         </div>
         @endforeach
